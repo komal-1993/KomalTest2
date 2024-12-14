ff26184db6a8125f8523d419c2cbd74736f0e00f
komal-1993 <komalkhopade5@gmail.com>
2024-12-14 11:41:06 +0530

    changes

--- src/practice/DemoA.java
+++ src/practice/DemoA.java
@@ -2,6 +2,6 @@ package practice;
 
 public class DemoA {
 	public static void main(String args[]) {
-		System.out.println("Hi java from B");
+		System.out.println("Hi java from B sdfsdfdsf");
 	}
 }
